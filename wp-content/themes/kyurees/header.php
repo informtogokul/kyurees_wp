1289df6258a8ca534bc9d5527025269a272cc4c7
gokulwebdeveloper <48559475+gokulwebdeveloper@users.noreply.github.com>
2024-02-08 11:36:45 +0530

    updating the ui

--- wp-content/themes/kyurees/header.php
+++ wp-content/themes/kyurees/header.php
@@ -11,11 +11,15 @@
     <script src="<?php echo esc_url(get_template_directory_uri()."/assets/js/color-modes.js"); ?>"></script>
     <link rel="canonical" href="<?php echo esc_url("https://getbootstrap.com/docs/5.3/examples/carousel/"); ?>">
     <link rel="stylesheet" href="<?php echo esc_url("https://cdn.jsdelivr.net/npm/@docsearch/css@3"); ?>">
+    <link rel="icon" type="image/x-icon" href="<?php echo esc_url(get_template_directory_uri()."/assets/images/favicon.png"); ?>">
     <link href="<?php echo esc_url(get_template_directory_uri()."/assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
 <!-- Custom styles for this template -->
     <link href="<?php echo esc_url(get_template_directory_uri()."/assets/css/custome.css"); ?>" rel="stylesheet">
      <?php wp_head(); ?>
      <link href="<?php echo esc_url(get_template_directory_uri()."/style.css"); ?>" rel="stylesheet">
+     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url(get_template_directory_uri()."/assets/images/apple-touch-icon.png"); ?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url(get_template_directory_uri()."/assets/images/favicon-32x32.png"); ?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url(get_template_directory_uri()."/assets/images/favicon-16x16.png"); ?>">
   </head>
 <body>
 
@@ -28,24 +32,25 @@
         </a>
       </div>
 
-      <div class="col-md-8">
+        <div class="col-md-8 order-md-1 order-last col-12">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-    
-        <?php
+          <?php
         wp_nav_menu( array(
                 'menu'              => 'primary',
                 'depth'             => 2,
                 'container'         => 'div',
                 'container_class'   => 'navbar-collapse collapse',
-                'menu_class'        => 'nav navbar-nav',
+                'menu_class'        => 'navbar-nav mx-auto mb-2 mb-md-0',
+                'menu_id' => 'navbarCollapse',
                 'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                 'walker'            => new wp_bootstrap_navwalker())
             );
         ?>
       </div>
-      <div class="col-md-2">
+      <div class="col-md-2 order-md-3">
+      <a class="btn btn-outline-pink float-end" href="https://learn.kyureeus.com/" type="submit" target="_blank">Student Login</a>
         <a class="btn btn-outline-pink float-end" href="<?php echo get_permalink('87'); ?>" type="submit">Contact Us</a>
       </div>
       
